cfeb75d4bb62d0156e992821bd08c73f4cf21dae
iyansr <ikhyan7@gmail.com>
2023-08-23 23:19:49 +0800

    g

--- apps/portal/next.config.js
+++ apps/portal/next.config.js
@@ -8,7 +8,16 @@ module.exports = {
     return [
       {
         source: '/api/:path*',
-        headers: [{ key: 'Access-Control-Allow-Origin', value: '*' }],
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' }, // replace this your actual origin
+          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
       },
     ];
   },
